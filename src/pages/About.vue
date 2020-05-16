999dc2f37f9834e68a75913f6d0f761a4c63146a
paul <pa.louyot@gmail.com>
2020-05-16 23:50:30 +0200

    Update homepage

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,14 @@
 <template>
   <Layout>
     <h1>About Paul</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <p>More coming soon...</p>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
+    title: 'About'
   }
 }
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,13 @@
 <template>
-  <Layout class="index-with-bg">
-    <div class="bg-container">
-
-    </div>
+  <Layout class="index-layout">
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <div>
-    <g-image alt="Example image" src="~/mypicture.png" width="135" />
+      <g-image alt="Example image" src="~/mypicture.png" width="135" />
     </div>
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <a href="https://github.com/paul-louyot/paul-louyot.github.io" target="_blank" rel="noopener">GitHub</a>
     </p>
 
   </Layout>
@@ -19,30 +16,25 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Home'
   }
 }
 </script>
 
 <style>
+.home-links {
+  position: absolute;
+  bottom: 0;
+}
+
 .home-links a {
   margin-right: 1rem;
 }
 
-.index-with-bg {
+.index-layout {
   height: 100vh;
   position: relative;
+  background-color: #F1F1F1;
 }
 
-.bg-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -2;
-  background-image: url(~@/bannerlaptop.jpg);
-  background-size: cover;
-  background-repeat: no-repeat;
-}
 </style>
