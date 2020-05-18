06a7d200645a4d95874ddb2c0f35ca6e5d76a1da
paul <pa.louyot@gmail.com>
2020-05-18 21:40:35 +0200

    Add meta tags to head

--- src/main.js
+++ src/main.js
@@ -4,6 +4,38 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
+
+  // Add meta tags
+  head.meta.push({
+    name: 'keywords',
+    content: 'HTML,CSS,JavaScript,Gridsome'
+  })
+
+  head.meta.push({
+    name: 'description',
+    content: 'Welcome to Paul Louyot\'s personal website'
+  })
+
+  head.meta.push({
+    property: 'og:title',
+    content: 'Paul\'s website'
+  })
+
+  head.meta.push({
+    property: 'og:description',
+    content: 'Welcome to Paul Louyot\'s personal website'
+  })
+
+  head.link.push({
+    rel: 'canonical',
+    content: 'https://paul-louyot.github.io/'
+  })
+
+  head.link.push({
+    name: 'robots',
+    content: 'index, follow'
+  })
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
