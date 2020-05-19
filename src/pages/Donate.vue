bff274c3ea5cfb7d73e6b745fb78704c446aef05
paul <pa.louyot@gmail.com>
2020-05-19 19:12:13 +0200

    Center btn-donate

--- src/pages/Donate.vue
+++ src/pages/Donate.vue
@@ -1,11 +1,11 @@
 <template>
   <Layout>
-  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-    <input type="hidden" name="cmd" value="_donations" />
-    <input type="hidden" name="business" value="3G4AZADX3M4RQ" />
-    <input type="hidden" name="currency_code" value="EUR" />
-    <input type="image" src="https://www.paypalobjects.com/en_US/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-    <img alt="" border="0" src="https://www.paypal.com/en_FR/i/scr/pixel.gif" width="1" height="1" />
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+      <input type="hidden" name="cmd" value="_donations" />
+      <input type="hidden" name="business" value="3G4AZADX3M4RQ" />
+      <input type="hidden" name="currency_code" value="EUR" />
+      <input class="btn-donate" type="image" src="https://www.paypalobjects.com/en_US/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+      <img alt="" border="0" src="https://www.paypal.com/en_FR/i/scr/pixel.gif" width="1" height="1" />
     </form>
   </Layout>
 </template>
@@ -19,4 +19,8 @@ export default {
 </script>
 
 <style>
+.btn-donate {
+  margin: 0 auto;
+  display: block;
+}
 </style>
