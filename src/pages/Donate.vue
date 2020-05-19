58b8ef3b09373142f6504b6c35d15bc3b9152a9b
paul <pa.louyot@gmail.com>
2020-05-19 18:44:42 +0200

    Add donation button

--- src/pages/Donate.vue
+++ src/pages/Donate.vue
@@ -1,6 +1,12 @@
 <template>
   <Layout>
-    Coming soon
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+    <input type="hidden" name="cmd" value="_donations" />
+    <input type="hidden" name="business" value="3G4AZADX3M4RQ" />
+    <input type="hidden" name="currency_code" value="EUR" />
+    <input type="image" src="https://www.paypalobjects.com/en_US/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+    <img alt="" border="0" src="https://www.paypal.com/en_FR/i/scr/pixel.gif" width="1" height="1" />
+    </form>
   </Layout>
 </template>
 
