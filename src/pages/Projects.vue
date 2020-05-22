66a8e02733e05cbbb14255d279d7ad3e3fc5aae4
paul <pa.louyot@gmail.com>
2020-05-22 23:54:09 +0200

    Diplay projects with cards

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,33 +1,47 @@
 <template>
   <Layout>
     <h1>Projects</h1>
-    <h2>This website</h2>
-    <a
-      href="https://github.com/paul-louyot/paul-louyot.github.io"
-      target="_blank"
-      rel="noopener"
-    >
-      Github
-      <!-- add illustration -->
-    </a>
-    <h2>Personal wiki</h2>
-    <a
-      href="https://paul-louyot.gitbook.io/wiki/"
-      target="_blank"
-      rel="noopener"
-    >
-      Website
-      <!-- add illustration -->
-    </a>
-    </br>
-    <a
-      href="https://github.com/paul-louyot/wiki"
-      target="_blank"
-      rel="noopener"
-    >
-      Github
-      <!-- add illustration -->
-    </a>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">This website</div>
+        <p class="text-gray-700 text-base">
+          This is my personal website.
+        </p>
+        <a
+          href="https://github.com/paul-louyot/paul-louyot.github.io"
+          target="_blank"
+          rel="noopener"
+        >
+          Github
+          <!-- add illustration -->
+        </a>
+      </div>
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Personal wiki</div>
+        <p class="text-gray-700 text-base">
+          This is my personal wiki.
+        </p>
+        <a
+          href="https://paul-louyot.gitbook.io/wiki/"
+          target="_blank"
+          rel="noopener"
+        >
+          Website
+          <!-- add illustration -->
+        </a>
+        </br>
+        <a
+          href="https://github.com/paul-louyot/wiki"
+          target="_blank"
+          rel="noopener"
+        >
+          Github
+          <!-- add illustration -->
+        </a>
+      </div>
+    </div>
   </Layout>
 </template>
 
