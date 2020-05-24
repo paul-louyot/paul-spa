544124bed28943b27ee7458393f8890186e1cadd
paul-louyot <44401972+paul-louyot@users.noreply.github.com>
2020-05-24 16:03:54 +0200

    Improve homepage (#2)
    * Add loader for map - wip
    
    * Add loader under image
    
    * Fix loader position

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1>Life abroad</h1>
-      <p>
+      <p class="my-2">
         <a
           href="https://www.google.com/maps/d/drive?state=%7B%22ids%22%3A%5B%221QwbJLPZxJFHC6lun0ZkGH7dgzHdnGT3m%22%5D%2C%22action%22%3A%22open%22%2C%22userId%22%3A%22116070659054577666496%22%7D&usp=sharing"
           target="_blank"
@@ -10,8 +10,18 @@
           Explore in detail
         </a>
       </p>
-      <div class="map-container">
-        <iframe class="map-abroad" src="https://www.google.com/maps/d/u/0/embed?mid=1QwbJLPZxJFHC6lun0ZkGH7dgzHdnGT3m" height="480"></iframe>
+      <div class="px-4">
+        <div class="map-container relative mx-auto">
+          <g-image
+            src="~/spinner.gif"
+            class="absolute loader w-20"
+          />
+          <iframe
+            class="map-abroad absolute z-10"
+            src="https://www.google.com/maps/d/u/0/embed?mid=1QwbJLPZxJFHC6lun0ZkGH7dgzHdnGT3m"
+            height="480"
+          />
+        </div>
       </div>
     <h1>Find me here</h1>
     <ul>
@@ -34,6 +44,13 @@
         </a>
       </li>
     </ul>
+    <p class="mt-4">
+      Built with
+      <a href="https://gridsome.org/" target="_blank" rel="noopener">Gridsome</a>
+      +
+      <a href="https://tailwindcss.com/" target="_blank" rel="noopener">Tailwind</a>
+      <!-- <a href="https://github.com/paul-louyot/paul-louyot.github.io" target="_blank" rel="noopener">Repo</a> -->
+    </p>
 
   </Layout>
 </template>
@@ -48,13 +65,24 @@ export default {
 
 <style scoped>
 .map-abroad {
-  width: 100%;
-  max-width: 640px;
   display: block;
+  width: 100%;
+  height: 100%;
   margin: 0 auto;
 }
 
 .map-container {
-  padding: 0 10px;
+  height: 480px;
+  max-width: 640px;
+}
+
+.loader{
+  left: calc(50% - 2.5rem)
+}
+
+@media screen and (min-height: 600px){
+  .loader{
+    top: calc(50% - 2.5rem)
+  }
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,13 @@
 <template>
-  <Layout class="index-layout">
+  <Layout class="index-layout sm:h-screen">
     <div>
       <g-image class="mx-auto" alt="Example image" src="~/mypicture.png" width="200" />
     </div>
+    <div class="text-center text-xl mt-6 text-blue-400 font-semibold">
+      Welcome to Paul's website
+    </div>
     <div class="pt-4 ">
-      <a href="/projects" class="btn btn-blue block mx-auto hover:translate-y-1">
+      <a href="/projects" class="btn btn-blue block mx-auto">
         Browse my projects
       </a>
       <a href="/check-this-out" class="btn btn-blue block mx-auto mt-2">
@@ -14,13 +17,6 @@
         Donate
       </a>
     </div>
-    <p class="home-links">
-      Built with
-      <a href="https://gridsome.org/" target="_blank" rel="noopener">Gridsome</a>
-      +
-      <a href="https://tailwindcss.com/" target="_blank" rel="noopener">Tailwind</a>
-      <!-- <a href="https://github.com/paul-louyot/paul-louyot.github.io" target="_blank" rel="noopener">Repo</a> -->
-    </p>
   </Layout>
 </template>
 
@@ -33,18 +29,9 @@ export default {
 </script>
 
 <style>
-.home-links {
-  position: absolute;
-  bottom: 0;
-}
-
 .index-layout {
-  position: relative;
-  height: 100vh;
-  min-height: 100vh;
-  min-height: -webkit-fill-available;
+  min-height: 570px;
 }
-
 </style>
 
 <style scoped>
